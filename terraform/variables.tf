de219064b433b5ade9d99000b54c5bb8714bb472
Jagendra Mishra <jagendramishra@Jagendras-Air.lan>
2025-03-15 08:40:02 +0530

    var changes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 # variable "azure_client_id" {
 #   type        = string
 #   description = "The client ID of the Azure Service Principal"
-#   default     = "069cd2c1-7f19-4ba3-8f5d-2c368bf439f6"
+#   default     = ""
 # }
 
 # variable "azure_client_secret" {
@@ -13,11 +13,11 @@
 # variable "azure_subscription_id" {
 #   type        = string
 #   description = "The subscription ID for Azure"
-#   default     = "dfb02290-cf46-4364-ab45-514e681a3cf9"
+#   default     = ""
 # }
 
 # variable "azure_tenant_id" {
 #   type        = string
 #   description = "The tenant ID for Azure"
-#   default     = "7f1ad8c5-dc6f-4202-98c0-9ef6e9c77ef3"
+#   default     = ""
 # }

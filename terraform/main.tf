9cdab0f93c42f61921c70a551750c49b68d58a34
Jagendra Atal Prakash <jagendra.prakash@mulberry.com>
2025-03-15 08:37:28 +0530

    initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.16.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "dfb02290-cf46-4364-ab45-514e681a3cf9"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "learnk8sResourceGroup"
+  location = "Central India"
+}
+
+resource "azurerm_kubernetes_cluster" "cluster" {
+  name                = "learnk8scluster"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "learnk8scluster"
+
+  default_node_pool {
+    name       = "default"
+    node_count = "2"
+    vm_size    = "standard_d2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  http_application_routing_enabled = "true"
+
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestoraccjagendra"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "example" {
+  name                  = "contentjagendra"
+  storage_account_id    = azurerm_storage_account.example.id
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "example" {
+  name                   = "my-awesome-content.zip"
+  storage_account_name   = azurerm_storage_account.example.name
+  storage_container_name = azurerm_storage_container.example.name
+  type                   = "Block"
+  source                 = "/Users/jagendramishra/projects/service.yaml"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+# variable "azure_client_id" {
+#   type        = string
+#   description = "The client ID of the Azure Service Principal"
+#   default     = "069cd2c1-7f19-4ba3-8f5d-2c368bf439f6"
+# }
+
+# variable "azure_client_secret" {
+#   type        = string
+#   description = "The client secret of the Azure Service Principal"
+#   default     = ""
+# }
+
+# variable "azure_subscription_id" {
+#   type        = string
+#   description = "The subscription ID for Azure"
+#   default     = "dfb02290-cf46-4364-ab45-514e681a3cf9"
+# }
+
+# variable "azure_tenant_id" {
+#   type        = string
+#   description = "The tenant ID for Azure"
+#   default     = "7f1ad8c5-dc6f-4202-98c0-9ef6e9c77ef3"
+# }
